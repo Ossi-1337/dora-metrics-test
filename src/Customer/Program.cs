454cc7df4f2605b5058213e756e529a80dfa6628
Ossi <oskarolsen7@gmail.com>
2025-11-19 11:27:18 +0100

    Update Program.cs

--- src/Customer/Program.cs
+++ src/Customer/Program.cs
@@ -4,5 +4,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => new { Service = "Customer", Status = "OK", Shared = SharedHelper.Ping() });
-
+// test
 app.Run();
